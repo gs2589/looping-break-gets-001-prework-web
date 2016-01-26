ed615c4d12e4dc03f83c69a170ecc1740be89a27
gs2589 <gabriel.seidner@gmail.com>
2016-01-26 16:25:45 -0500

    Done.

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,14 @@
 
 def levitation_quiz
-	#your code here
+
+loop do 
+  puts "What is the spell that enacts levitation?"
+  response=gets.chomp
+  if response=="Wingardium Leviosa" then break
+  end
+end
+puts "You passed the quiz!"
 end
 
 
+#levitation_quiz
